96754ed784bd6ebda62c8d6f5ae41a8ceacc8d92
Hoang Dinh <d.conghoang@gmail.com>
2024-02-13 15:55:54 +0700

    Update algokit.rb

--- Casks/algokit.rb
+++ Casks/algokit.rb
@@ -1,8 +1,9 @@
 cask "algokit" do
+  arch arm: "arm64", intel: "X64"
   version "1.12.6"
   sha256 "b53cd0990253a9a7d35444562b97198feeb630d7f15fb63a4f5496e06bc7f6e7"
 
-  url "https://github.com/PatrickDinh/algokit-cli/releases/download/v#{version}/algokit-v#{version}-macos-py3.12.zip"
+  url "https://github.com/PatrickDinh/algokit-cli/releases/download/v#{version}/algokit-v#{version}-macos-#{arch}-py3.12.zip"
   name "algokit"
   desc "Algorand development kit command-line interface"
   homepage "https://github.com/PatrickDinh/algokit-cli"
