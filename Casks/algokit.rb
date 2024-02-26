88b024c21b3d1fe1ade1cd127308c00d88cdb675
Hoang Dinh <patrick.dinh@makerx.com.au>
2024-02-26 13:19:17 +1000

    Update algokit.rb

--- Casks/algokit.rb
+++ Casks/algokit.rb
@@ -1,9 +1,9 @@
 cask "algokit" do
-  path arm: "dist/algokit-2.0.6-macos_arm64.tar.gz", intel: "dist/algokit-2.0.6-macos_x64.tar.gz"
+  binary_path arm: "dist/algokit-2.0.6-macos_arm64.tar.gz", intel: "dist/algokit-2.0.6-macos_x64.tar.gz"
   version "2.0.6"
   sha256 arm: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5", intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/PatrickDinh/algokit-cli/releases/download/#{version}/#{path}"
+  url "https://github.com/PatrickDinh/algokit-cli/releases/download/#{version}/#{binary_path}"
   name "algokit"
   desc "Algorand development kit command-line interface"
   homepage "https://github.com/PatrickDinh/algokit-cli"
